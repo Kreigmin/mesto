a767b41f490b2ce187cac6f3741d51846809c172
Dmitry Sobolev <leejjuk@gmail.com>
2021-04-13 21:15:38 +0300

    feat: сделать сброс input при закрытии popup

--- blocks/popup/__job/popup__job.js
+++ blocks/popup/__job/popup__job.js
@@ -1,6 +1,12 @@
 let inputJob = document.querySelector('.popup__job');
-let profileJob = document.querySelector('.profile__job').textContent;
+let profileJob = document.querySelector('.profile__job');
 
 editBtn.addEventListener('click', function() {
-  inputJob.setAttribute('value', profileJob);
+  inputJob.setAttribute('value', profileJob.textContent);
+});
+
+let defaultTextJob = profileJob.textContent;
+
+closeBtn.addEventListener('click', function() {
+  inputJob.value = defaultTextJob;
 });
--- blocks/popup/__name/popup__name.js
+++ blocks/popup/__name/popup__name.js
@@ -1,7 +1,13 @@
 let inputName = document.querySelector('.popup__name');
-let profileName = document.querySelector('.profile__name').textContent;
+let profileName = document.querySelector('.profile__name');
 
 editBtn.addEventListener('click', function() {
-  inputName.setAttribute('value', profileName);
+  inputName.setAttribute('value', profileName.textContent);
+});
+
+let defaultTextName = profileName.textContent;
+
+closeBtn.addEventListener('click', function() {
+  inputName.value = defaultTextName;
 });
 
