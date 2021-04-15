ec5fc4477fff4cfd85d875203d49386d39490052
Dmitry Sobolev <leejjuk@gmail.com>
2021-04-15 11:44:11 +0300

    refactor: Вынести работу кнопок в отдельные функции и удалить лишнее

--- scripts/script.js
+++ scripts/script.js
@@ -1,54 +1,38 @@
-//Открытие popup
-let editBtn = document.querySelector('.profile__edit-btn');
+//Объявление переменных
+let editBtn = document.querySelector('.profile__edit-btn');// Выбор кнопки редактирования
+let inputName = document.querySelector('.change-form_name_value');// Выбор поля ввода имени
+let profileName = document.querySelector('.profile__name');// Выбор имени
+let inputJob = document.querySelector('.change-form_job_value');// Выбор поля ввода профессии
+let profileJob = document.querySelector('.profile__job');// Выбор профессии
+let popup = document.querySelector('.popup');// Выбор pop-up'a
+let closeBtn = document.querySelector('.popup__close-btn');// Выбор кнопки закрытия popup
+let formElement = document.querySelector('.change-form');// Выбор формы
 
-editBtn.addEventListener('click', function() {
-  document.querySelector('.popup').classList.toggle('popup_opened');
-});
-
-//Закрытие popup
-let closeBtn = document.querySelector('.popup__close-btn');
-
-closeBtn.addEventListener('click', function() {
-  document.querySelector('.popup').classList.toggle('popup_opened');
-});
-
-//Начальный текст в input
-let inputName = document.querySelector('.popup__name');
-let profileName = document.querySelector('.profile__name');
-
-editBtn.addEventListener('click', function() {
-  inputName.setAttribute('value', profileName.textContent);
-});
-
-closeBtn.addEventListener('click', function() {
-  let defaultTextName = profileName.textContent;
-  inputName.value = defaultTextName;
-});
-
-let inputJob = document.querySelector('.popup__job');
-let profileJob = document.querySelector('.profile__job');
-
-editBtn.addEventListener('click', function() {
-  inputJob.setAttribute('value', profileJob.textContent);
-});
-
-closeBtn.addEventListener('click', function() {
+//Функция открытия popup и добавление данных в input
+function popupOpen() {
+  if (popup.classList.contains('popup_opened') !== true) {// Условие если не равно
+    popup.classList.add('popup_opened');// Добавить класс
+    inputName.setAttribute('value', profileName.textContent);// Добавить имя в input при открытии
+    inputJob.setAttribute('value', profileJob.textContent);// Добавить профессию в input при открытии
+  }
+}
+//Функция закрытия popup
+function popupClose() {
+  popup.classList.remove('popup_opened');
   let defaultTextJob = profileJob.textContent;
   inputJob.value = defaultTextJob;
-});
-
-//Отправка и закрытие формы
-let formElement = document.querySelector('.popup__container');
-let submitBtn = document.querySelector('.popup__submit-btn');
-
+  let defaultTextName = profileName.textContent;
+  inputName.value = defaultTextName;
+}
+//Функция отправки формы
 function formSubmitHandler(evt) {
   evt.preventDefault();
   let kak = profileName.textContent = inputName.value;
   profileJob.textContent = inputJob.value;
+  popupClose();
 }
 
-formElement.addEventListener('submit', formSubmitHandler);
+editBtn.addEventListener('click', popupOpen);// открытие формы и добавление данных в input
+closeBtn.addEventListener('click', popupClose);// закрытие формы
+formElement.addEventListener('submit', formSubmitHandler);// отправка формы
 
-submitBtn.addEventListener('click', function() {
-  document.querySelector('.popup').classList.toggle('popup_opened');
-});
