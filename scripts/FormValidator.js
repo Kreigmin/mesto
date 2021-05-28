d112ab71f888792a1cf5daecbfda685d5e0d503c
Dmitry Sobolev <leejjuk@gmail.com>
2021-05-28 20:57:24 +0300

    refactor: удалить не нужные аргументы методов в классе FormValidator

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -14,17 +14,17 @@ export class FormValidator {
   }
 
   //метод показа ошибки валидации-----------------------------------------------------------------------------------------
-  _showInputError(fieldset, inputElement, errorMessage, inputErrorClass, errorClass)  {
+  _showInputError(fieldset, inputElement, errorMessage)  {
     const errorElement = fieldset.querySelector(`.${inputElement.id}-error`);
-    inputElement.classList.add(inputErrorClass);
+    inputElement.classList.add(this._inputErrorClass);
     errorElement.textContent = errorMessage;
-    errorElement.classList.add(errorClass);
+    errorElement.classList.add(this._errorClass);
   }
   //метод скрытия ошибки валидации-----------------------------------------------------------------------------------------
-  _hideInputError(fieldset, inputElement, inputErrorClass, errorClass) {
+  _hideInputError(fieldset, inputElement) {
     const errorElement = fieldset.querySelector(`.${inputElement.id}-error`);
-    inputElement.classList.remove(inputErrorClass);
-    errorElement.classList.remove(errorClass);
+    inputElement.classList.remove(this._inputErrorClass);
+    errorElement.classList.remove(this._errorClass);
     errorElement.textContent = '';
   }
 
@@ -34,56 +34,55 @@ export class FormValidator {
     });
   }
   // метод проверки полей на валидность-----------------------------------------------------------------------------------------
-  _checkInputValidity(fieldset, inputElement, inputErrorClass, errorClass)  {
+  _checkInputValidity(fieldset, inputElement)  {
     if (!inputElement.validity.valid) {
-      this._showInputError(fieldset, inputElement, inputElement.validationMessage, inputErrorClass, errorClass);
+      this._showInputError(fieldset, inputElement, inputElement.validationMessage);
     } else {
-      this._hideInputError(fieldset, inputElement, inputErrorClass, errorClass);
+      this._hideInputError(fieldset, inputElement);
     }
   }
 
   //метод преключения кнопки submit-----------------------------------------------------------------------------------------
   _toggleButtonState(
     inputList,
-    buttonElement,
-    inactiveButtonClass) {
+    buttonElement) {
       if (this._hasInvalidInput(inputList)) {
-        buttonElement.classList.add(inactiveButtonClass);
+        buttonElement.classList.add(this._inactiveButtonClass);
         buttonElement.setAttribute('disabled', true);
       } else {
-        buttonElement.classList.remove(inactiveButtonClass);
+        buttonElement.classList.remove(this._inactiveButtonClass);
         buttonElement.removeAttribute('disabled');
       }
     }
+
+    _handleClearErrorsAndToggleButton(fieldset, inputs, button) {
+      this._clearAllInputs(fieldset, this._inputSelector, this._inputErrorClass, this._errorClass);
+      this._toggleButtonState(inputs, button);
+    }
+
   // метод добавления слушателей-----------------------------------------------------------------------------------------
-  _setEventListeners(fieldset,
-    submitButtonSelector,
-    inputSelector,
-    inactiveButtonClass,
-    inputErrorClass,
-    errorClass) {
-      const inputList = Array.from(fieldset.querySelectorAll(inputSelector));
-      const buttonElement = fieldset.querySelector(submitButtonSelector);
+  _setEventListeners(fieldset) {
+      const inputList = Array.from(fieldset.querySelectorAll(this._inputSelector));
+      const buttonElement = fieldset.querySelector(this._submitButtonSelector);
       inputList.forEach((inputElement) => {
         inputElement.addEventListener('input', () => {
-          this._checkInputValidity(fieldset, inputElement, inputErrorClass, errorClass);
-          this._toggleButtonState(inputList, buttonElement, inactiveButtonClass);
+          this._checkInputValidity(fieldset, inputElement, this._inputErrorClass, this._errorClass);
+          this._toggleButtonState(inputList, buttonElement);
         });
       });
       editBtn.addEventListener('click', () => {
-        this._clearAllInputs(fieldset,inputSelector, inputErrorClass, errorClass);
+        this._handleClearErrorsAndToggleButton(fieldset, inputList, buttonElement);
       });
       addBtn.addEventListener('click', () => {
-        this._clearAllInputs(fieldset,inputSelector, inputErrorClass, errorClass);
-        this._toggleButtonState(inputList, buttonElement, inactiveButtonClass);
+        this._handleClearErrorsAndToggleButton(fieldset, inputList, buttonElement);
       });
     }
 
   //метод очистки полей при открытии формы-----------------------------------------------------------------------------------------
-  _clearAllInputs(fieldset, inputSelector, inputErrorClass, errorClass) {
-    const inputList = fieldset.querySelectorAll(inputSelector);
+  _clearAllInputs(fieldset) {
+    const inputList = fieldset.querySelectorAll(this._inputSelector);
     inputList.forEach((item) => {
-      this._hideInputError(fieldset, item, inputErrorClass, errorClass);
+      this._hideInputError(fieldset, item);
     });
   }
   //метод включения валидации-----------------------------------------------------------------------------------------
@@ -93,13 +92,7 @@ export class FormValidator {
       evt.preventDefault();
     });
     const fieldset = form.querySelector(this._fieldsetSelector);
-    this._setEventListeners(
-      fieldset,
-      this._submitButtonSelector,
-      this._inputSelector,
-      this._inactiveButtonClass,
-      this._inputErrorClass,
-      this._errorClass);
+    this._setEventListeners(fieldset);
   }
 }
 
