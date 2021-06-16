0c6dde67c03cbda8eaa897303721c2e067ac5a03
Dmitry Sobolev <leejjuk@gmail.com>
2021-06-16 20:59:15 +0300

    refactor: add fields name and job to class UserInfo constructor

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -2,11 +2,13 @@ export default class UserInfo {
   constructor({profileNameSelector, profileJobSelector}) {
     this._profileNameSelector = profileNameSelector;
     this._profileJobSelector = profileJobSelector;
+    this._name = document.querySelector(this._profileNameSelector);
+    this._job = document.querySelector(this._profileJobSelector);
   }
 
   getUserInfo() {
-    const name = document.querySelector(this._profileNameSelector);
-    const job = document.querySelector(this._profileJobSelector);
+    const name = this._name;
+    const job = this._job;
     const data = {name, job};
     return data;
   }
